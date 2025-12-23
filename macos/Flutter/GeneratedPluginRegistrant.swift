b87b43a7cb8d05dc4c6a8d3cad8164cd6c227e19
niladri das <125604915+bniladridas@users.noreply.github.com>
2025-12-24 04:40:40 +0530

    chore: remove unused deps (#101)
    Co-authored-by: Niladri Das <bniladridas@users.noreply.github.com>

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -8,7 +8,6 @@ import Foundation
 import path_provider_foundation
 import screen_retriever_macos
 import shared_preferences_foundation
-import url_launcher_macos
 import webview_flutter_wkwebview
 import window_manager
 
@@ -16,7 +15,6 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   ScreenRetrieverMacosPlugin.register(with: registry.registrar(forPlugin: "ScreenRetrieverMacosPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
-  UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
   WebViewFlutterPlugin.register(with: registry.registrar(forPlugin: "WebViewFlutterPlugin"))
   WindowManagerPlugin.register(with: registry.registrar(forPlugin: "WindowManagerPlugin"))
 }
