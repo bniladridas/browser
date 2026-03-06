870561e3cfd649ee7b0e8ed28dbf8da0abbf7543
Niladri Das <bniladridas@users.noreply.github.com>
2026-03-07 03:46:57 +0530

    fix: remove unused test import

--- integration_test/app_test.dart
+++ integration_test/app_test.dart
@@ -7,7 +7,6 @@
 import 'dart:io';
 
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:integration_test/integration_test.dart';
 import 'package:browser/main.dart';
